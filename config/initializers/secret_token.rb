a90da0f501cd08ecb181a3abf439813b0438f3c6
Jim Fowler <fowler@math.osu.edu>
2013-04-01 13:58:30 -0400

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gratisu::Application.config.secret_token = '4aeb1e6ae2067d3acc782a766ca254fdf3406f5998bf83e17935da493f421bc205f68f9b8eb43564141f92e51ffa75496768df321427340a0268bec0aee3c903'
